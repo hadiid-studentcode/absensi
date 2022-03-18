df2fa5ea010fe9e58e63de79b8dcb8669de8547c
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2022-03-18 11:49:21 +0700

    update

--- home.php
+++ home.php
@@ -56,10 +56,39 @@ if (isset($_GET['submit'])) {
     <link rel="stylesheet" href="dir/home.css">
 
     <title>Aplikasi Absensi PC IPM TAMPAN</title>
+    <style type="text/css">
+        .preloader {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 9999;
+            background-color: #fff;
+        }
+
+        .preloader .loading {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            font: 14px arial;
+        }
+
+       
+    </style>
 
 </head>
 
 <body>
+
+    <div class="preloader">
+        <div class="loading">
+            <img src="dir/images/logo.png" width="80">
+            <p class="text-center fw-bold">Loading</p>
+        </div>
+    </div>
+    
     <div class="container">
 
         <ul class="nav justify-content-end">
@@ -166,6 +195,12 @@ if (isset($_GET['submit'])) {
                 $("table").wrap("<div class='table-responsive'></div>");
                 $("table").addClass("table");
             });
+        </script>
+
+        <script>
+            $(document).ready(function() {
+                $(".preloader").fadeOut();
+            })
         </script>
 
 
